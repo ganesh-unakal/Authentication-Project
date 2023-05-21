9791e3a6953c1ee676b5e1ac21d23468527f78d8
Ganesh Unakal <122563639+ganesh-unakal@users.noreply.github.com>
2023-05-21 12:59:03 +0530

    Auto-logout

--- store/Auth-context.js
+++ store/Auth-context.js
@@ -1,38 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
 
-const AuthConetxt= React.createContext({
-    token: " ",
-    isLoggedIn: false,
-    login : (token)=>{},
-    logout: () => {}
-})
+export const AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
+  const [logoutTimer, setLogoutTimer] = useState(null);
 
- export const AuthContextProvider = (props) =>{
-    const initialToken = localStorage.getItem('token')
-   const [token, setToken]= useState(initialToken)
-  
+  const userIsLoggedIn = !!token; // if the token is a non-empty string, it will return 'true'; if the token is an empty string or null, it will return 'false'
 
-   const userIsLoggedIn = !!token // if the token is string not empty this will retuen 'true' if the token string and empty it will return false
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+  };
 
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
 
-    const loginHandler = (token) =>{  
-        setToken(token)
-        localStorage.setItem('token' , token)
-    };
+  // Function to start the logout timer
+  const startLogoutTimer = () => {
+    const timer = setTimeout(logoutHandler, 5 * 60 * 1000); // 5 minutes in milliseconds
+    setLogoutTimer(timer);
+  };
 
-    const logoutHandler = () =>{
-        setToken(null)
-        localStorage.removeItem('token')
-    };
+  // Function to reset the logout timer
+  const resetLogoutTimer = () => {
+    clearTimeout(logoutTimer);
+    startLogoutTimer();
+  };
 
-    const conetxtValue = {
-        token : token,
-        isLoggedIn: userIsLoggedIn,
-        login : loginHandler,
-        logout : logoutHandler
+  useEffect(() => {
+    if (userIsLoggedIn) {
+      startLogoutTimer();
+    } else {
+      clearTimeout(logoutTimer);
     }
+  }, [userIsLoggedIn]); // Run the effect whenever the user's login status changes
 
-    return <AuthConetxt.Provider value={conetxtValue}>{props.children}</AuthConetxt.Provider>
-}
-export default AuthConetxt;
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
